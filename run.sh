838b10bcf55e69b738960f39472006d4bf162ff2
w6688j <953372680@qq.com>
2018-11-26 13:39:41 +0800

    modify the run.sh file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-THEANO_FLAGS='floatX=float32,device=gpu1,nvcc.fastmath=True' python VarNDrr.py config.py
+THEANO_FLAGS='floatX=float32,device=cuda,nvcc.fastmath=True' python VarNDrr.py config.py
