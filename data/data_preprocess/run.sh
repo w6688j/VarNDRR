8ec4b03b0b55c5c8121e427e2d9c25b1852fb379
w6688j <953372680@qq.com>
2018-11-26 15:35:17 +0800

    generate data

--- data/data_preprocess/run.sh
+++ data/data_preprocess/run.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-process=../script/data_preprocess.py
-split=../script/data_split.py
+process=./script/data_preprocess.py
+split=./script/data_split.py
 
-data=../data/
+data=./data/
 
 mkdir work
 cd work
 
 # split dataset
-$split train $data/train
-$split test $data/test
-$split dev $data/dev
+python $split train $data/train
+python $split test $data/test
+python $split dev $data/dev
 
 # preprocess
-$process 4 train train.arg1 train.arg2 train.lbl
-$process 4 dev dev.arg1 dev.arg2 dev.lbl arg1.vcb.pkl arg2.vcb.pkl
-$process 4 test test.arg1 test.arg2 test.lbl arg1.vcb.pkl arg2.vcb.pkl
+python $process 4 train train.arg1 train.arg2 train.lbl
+python $process 4 dev dev.arg1 dev.arg2 dev.lbl arg1.vcb.pkl arg2.vcb.pkl
+python $process 4 test test.arg1 test.arg2 test.lbl arg1.vcb.pkl arg2.vcb.pkl
 
 rm *.arg1 *.arg2 *.lbl
